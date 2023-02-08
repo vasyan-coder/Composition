752ca0be38e48eb3730246c7c024d344746780db
Vasyan <vasyan.coder@gmail.com>
2023-02-08 10:44:04 +0300

    feat: create entities

---
+++ app/src/main/java/com/vasyancoder/composition/domain/entity/GameResult.kt
@@ -0,0 +1,8 @@
+package com.vasyancoder.composition.domain.entity
+
+data class GameResult(
+    val winner: Boolean,
+    val countOfRightAnswers: Int,
+    val countOfQuestions: Int,
+    val gameSettings: GameSettings,
+)
---
+++ app/src/main/java/com/vasyancoder/composition/domain/entity/Level.kt
@@ -0,0 +1,6 @@
+package com.vasyancoder.composition.domain.entity
+
+enum class Level {
+
+    TEST, EASY, NORMAL, HARD
+}
